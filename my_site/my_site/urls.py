6226b552dbce71e687550f2ef55d036f6d44f1b1
Joniha <joni.ha@icloud.com>
2014-10-03 19:54:21 +0300

    Update urls.py

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('my_site.myapp.views',
-	url(r'^$', 'run', name='run'),
-	url(r'^modifies/$', 'get', name='my'),
+    url(r'^$', 'run', name='run'),
+    url(r'^modifies/$', 'get', name='my'),
     url(r'^modifies/up$', 'update', name='update'),
     url(r'^modifies/in/', 'insert', name='insert'),
     url(r'^admin/', include(admin.site.urls)),
